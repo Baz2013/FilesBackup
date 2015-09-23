da552dd4690f836ba347de2c96052191fac0efed
bazar_from_linux <chuan.gu6@gmail.com>
2015-09-23 11:32:20 +0800

    modified:   backup_file.sh 	modified:   conf.sh

--- backup_file.sh
+++ backup_file.sh
@@ -26,7 +26,49 @@ writeLog(){
 	echo "${cur_time}|$1" >> ./backup_file.${log_name}
 }
 
+###检查配置脚本中配置的参数是否存在错误
+###如,配置的目录,文件是否存在
+###返回0表示参数配置正确,非0数字表示参数存在错误
+check_param(){
+	if [ -n "${SOURCE_DIRS}" ];then
+		echo "${SOURCE_DIRS}"|while read line
+		do
+			if [ ! -d "${line}" ];then
+				#echo "配置中的目录${line}不存在"
+				writeLog "配置中的目录${line}不存在"
+				echo "1"
+				return
+			fi
+		done
+	fi
 
+	if [ -n "${SOURCE_FILES}" ];then
+		echo "${SOURCE_FILES}"|while read line
+		do
+			if [ ! -d "${line}" ];then
+				writeLog "配置中的文件${line}不存在"
+				echo "2"
+				return
+			fi
+		done
+	fi
+
+	#检查TIMES是否是数字
+	s=$(expr ${TIMES} + 0 2>&1 >/dev/null)
+	if [ "$?" -ne "0" ];then
+		writeLog "TIMES 参数不是数字"
+		echo "3"
+		return
+	fi
+
+	if [ ! -d "${BACKUP_DIR}" ];then
+		writeLog "${BACKUP_DIR}目录不存在"
+		echo "4"
+		return
+	fi
+
+echo "0"
+}
 
 ##############################
 
@@ -51,7 +93,7 @@ else
 		writeLog "配置脚本没有可执行权限"
 		exit
 	else
-		source ${conf_file}
+		source ${conf_file} ##bug 1 ,加载失败时脚本不能够停止
 		echo "$?"
 		if [ $? -ne "0" ];then
 			echo "加载脚本失败 "
@@ -62,4 +104,6 @@ else
 fi
 
 
-
+#配置参数检查
+s=$(check_param)
+echo "s:${s}"
--- conf.sh
+++ conf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash - 
 ##≈‰÷√≤ø∑÷
-ddddd
-SOURCE_DIR=""
-SOURCE_FILES=""
-TIMES=180 ##s
-BACKUP_DIR=""
+##ddddd
+SOURCE_DIRS=""
+SOURCE_FILES="${HOME}/etc/aa.cfg
+${HOME}/etc/bb.cfg
+${HOME}/etc/cc.cfg"
+TIMES=l80 ##s
+BACKUP_DIR="${HOME}/back_up"
